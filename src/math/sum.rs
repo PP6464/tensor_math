b3013226556e3715fbc1198b8296f7bac8a588db
PP6464 <panthhp@outlook.com>
2026-07-09 13:09:39 +0100

    add multithreaded sum and trace

--- src/math/sum.rs
+++ src/math/sum.rs
@@ -1,18 +1,27 @@
+use rayon::iter::ParallelIterator;
 use crate::definitions::errors::TensorErrors;
 use crate::definitions::matrix::Matrix;
 use crate::definitions::tensor::Tensor;
 use num::Zero;
 use std::ops::Add;
+use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator};
 
 impl<T: Add<Output = T> + Clone + Zero> Tensor<T> {
-    /// Compute the sum of a `Tensor`
+    /// Compute the sum of a tensor
     pub fn sum(&self) -> T {
         self.iter().cloned().fold(T::zero(), |acc, x| acc + x)
     }
 }
 
+impl<T: Add<Output = T> + Clone + Zero + Send + Sync> Tensor<T> {
+    /// Compute the sum of a tensor
+    pub fn sum_mt(&self) -> T {
+        self.par_iter().cloned().reduce(|| T::zero(), |acc, x| acc + x)
+    }
+}
+
 impl<T: Add<Output = T> + Clone + Zero> Matrix<T> {
-    /// Compute the sum of a `Matrix`
+    /// Compute the sum of a matrix
     pub fn sum(&self) -> T {
         self.tensor.sum()
     }
@@ -28,6 +37,27 @@ impl<T: Add<Output = T> + Clone + Zero> Matrix<T> {
         for i in 0..self.shape.0.iter().min().unwrap().clone() {
             sum = sum.add(self[&[i, i]].clone());
         }
+
+        Ok(sum)
+    }
+}
+
+impl<T: Add<Output = T> + Clone + Zero + Send + Sync> Matrix<T> {
+    /// Compute the sum of a matrix
+    pub fn sum_mt(&self) -> T {
+        self.tensor.sum_mt()
+    }
+
+    /// Computes the trace of a matrix
+    pub fn trace_mt(self: &Matrix<T>) -> Result<T, TensorErrors> {
+        if !self.is_square() {
+            return Err(TensorErrors::NonSquareMatrix);
+        }
+
+        let sum = (0..self.rows)
+            .into_par_iter()
+            .map(|i| self[&[i, i]].clone())
+            .reduce(|| T::zero(), |acc, x| acc + x);
 
         Ok(sum)
     }
--- tests/math/sum.rs
+++ tests/math/sum.rs
@@ -45,4 +45,44 @@ mod sum_tests {
         let matrix = Matrix::<f64>::new(0, 0, vec![]).unwrap();
         assert_eq!(matrix.sum(), 0.0);
     }
+
+    #[test]
+    fn test_tensor_sum_mt() {
+        let tensor = Tensor::new(&shape![2, 2], vec![1, 2, 3, 4]).unwrap();
+        assert_eq!(tensor.sum_mt(), 10);
+    }
+
+    #[test]
+    fn test_matrix_sum_mt() {
+        let matrix = vec![1.0, 2.0, 3.0, 4.0].into_iter().collect::<Matrix<_>>();
+        assert_eq!(matrix.sum_mt(), 10.0);
+    }
+
+    #[test]
+    fn test_matrix_trace_mt() {
+        let matrix = Matrix::new(2, 2, vec![1, 2, 3, 4]).unwrap();
+        assert_eq!(matrix.trace_mt().unwrap(), 5);
+    }
+
+    #[test]
+    fn test_matrix_trace_non_square_mt() {
+        let matrix = Matrix::new(2, 3, vec![1, 2, 3, 4, 5, 6]).unwrap();
+        let result = matrix.trace_mt();
+        assert!(matches!(result, Err(TensorErrors::NonSquareMatrix)));
+    }
+
+    #[test]
+    fn zero_by_zero_trace_mt() {
+        let matrix = Matrix::<usize>::new(0, 0, vec![]).unwrap();
+        assert_eq!(matrix.trace_mt().unwrap(), 0);
+    }
+
+    #[test]
+    fn empty_matrix_tensor_sum_mt() {
+        let tensor = Tensor::<i32>::new(&shape![0], vec![]).unwrap();
+        assert_eq!(tensor.sum_mt(), 0);
+
+        let matrix = Matrix::<f64>::new(0, 0, vec![]).unwrap();
+        assert_eq!(matrix.sum_mt(), 0.0);
+    }
 }
