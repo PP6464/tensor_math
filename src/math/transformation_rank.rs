7499d9506a99101a380b87158bf31e38a0b8d42c
PP6464 <panthhp@outlook.com>
2026-09-08 12:23:02 +0100

    update docs

--- src/math/transformation_rank.rs
+++ src/math/transformation_rank.rs
@@ -4,22 +4,21 @@ use num::complex::{Complex64, ComplexFloat};
 
 impl Matrix<f64> {
     /// Gives the rank of the transformation represented by this matrix.
-    /// Note that `mat.rank()` will just give 2, since `Matrix` can be
-    /// dereferenced into a `Tensor`, so will just inherit `rank` from there,
-    /// but this function will give the desired result.
     pub fn transformation_rank(&self) -> usize {
         let mut all_zero_rows = 0usize;
         let ref_form = self.row_echelon();
         let rows = self.rows;
 
-        for i in (0..rows).rev() {
-            let row = ref_form.slice(i..i + 1, 0..self.cols).unwrap();
+        unsafe {
+            for i in (0..rows).rev() {
+                let row = ref_form.slice_unchecked(i..i + 1, 0..self.cols);
 
-            if !row.iter().all(|x| approx_eq!(f64, *x, 0.0)) {
-                return rows - all_zero_rows;
+                if !row.iter().all(|x| approx_eq!(f64, *x, 0.0)) {
+                    return rows - all_zero_rows;
+                }
+
+                all_zero_rows += 1;
             }
-
-            all_zero_rows += 1;
         }
 
         0
@@ -28,22 +27,21 @@ impl Matrix<f64> {
 
 impl Matrix<Complex64> {
     /// Gives the rank of the transformation represented by this matrix.
-    /// Note that `mat.rank()` will just give 2, since `Matrix` can be
-    /// dereferenced into a `Tensor`, so will just inherit `rank` from there,
-    /// but this function will give the desired result.
     pub fn transformation_rank(&self) -> usize {
         let mut all_zero_rows = 0usize;
         let ref_form = self.row_echelon();
         let rows = self.rows;
 
-        for i in (0..rows).rev() {
-            let row = ref_form.slice(i..i + 1, 0..self.cols).unwrap();
+        unsafe {
+            for i in (0..rows).rev() {
+                let row = ref_form.slice_unchecked(i..i + 1, 0..self.cols);
 
-            if !row.iter().all(|x| approx_eq!(f64, x.abs(), 0.0)) {
-                return rows - all_zero_rows;
+                if !row.iter().all(|x| approx_eq!(f64, x.abs(), 0.0)) {
+                    return rows - all_zero_rows;
+                }
+
+                all_zero_rows += 1;
             }
-
-            all_zero_rows += 1;
         }
 
         0
